70d9ac143cd41002b369f8b19b40ed2af4a6c7c5
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-04-11 20:47:36 +0200

    added extra

--- details.js
+++ details.js
@@ -1,24 +1,70 @@
+// contrasted text
+
+const contrastText = function (hexcolor) {
+  hexcolor = hexcolor.replace('#', '')
+  let r = parseInt(hexcolor.substr(0, 2), 16)
+  let g = parseInt(hexcolor.substr(2, 2), 16)
+  let b = parseInt(hexcolor.substr(4, 2), 16)
+  let yiq = (r * 299 + g * 587 + b * 114) / 1000
+  return yiq >= 128 ? 'black' : 'white'
+}
+//manage url
 const params = new URLSearchParams(window.location.search)
 const imageId = params.get('id')
 
+const URL = 'https://api.pexels.com/v1/photos/'
+const API_KEY = 'xFBj2MpOtMvpxx12EkG31qsXeIc43DZXkQVqOM5STgLlvhYYTHKhXdBl'
+
 window.addEventListener('DOMContentLoaded', () => {
-  fetch(URL + id).then((response) => {
-    const col = document.createElement('div')
-    col.classList.add('col-md-6')
-    const card = document.createElement('div')
-    card.classList.add('card', 'mb-5', 'shadow-sm')
+  if (imageId) {
+    const row = document.getElementById('card-row')
+    fetch(`${URL}${imageId}`, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const col = document.createElement('div')
+        col.classList.add('col-md-6')
+        const card = document.createElement('div')
+        card.classList.add('card', 'my-5', 'shadow-sm')
 
-    const { _id, name, photographer, photographer_url } = picObj
+        card.innerHTML = `<img src=${data.src.landscape} class="bd-placeholder-img card-img-top" />
+      <div class="card-body">
+          <h5 class="card-title">${data.alt}</h5>
+          <p class="card-text">
+              ${data.photographer}
+          </p>
+          <div class="d-flex justify-content-between align-items-center">
+          <a href="${data.photographer_url}" target="_blank" class="underline align-middle" rel="noopener noreferrer"><small class="text-muted align-top"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right align-top me-3" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
+          <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"/>
+        </svg>Photographer's Page</small></a>
+          </div>
+      </div>`
 
-    card.innerHTML = `<img src=${photos.src.portrait} class="bd-placeholder-img card-img-top" />
-    <div class="card-body">
-        <h5 class="card-title">${photos.alt}</h5>
-        <p class="card-text">
-            ${photos.photographer}
-        </p>
-        <div class="d-flex justify-content-between align-items-center">
-            <small class="text-muted">${photographer_url}</small>
-        </div>
-    </div>`
-  })
+        card.classList.add('frosted-glass')
+        col.appendChild(card)
+        row.appendChild(col)
+
+        const body = document.querySelector('body')
+        body.style.backgroundColor = data.avg_color
+
+        //colored jumbotron
+        const textColor = contrastText(data.avg_color)
+        const jumbotron = document.querySelector('.jumbotron')
+        jumbotron.style.color = textColor
+
+        const buttons = jumbotron.querySelectorAll('.btn')
+
+        buttons.forEach((button) => {
+          button.style.color = textColor
+          button.style.borderColor = textColor
+        })
+      })
+      .catch((error) => console.log('Error fetching picture details', error))
+  } else {
+    console.log('No image Id')
+  }
 })
--- pexels.js
+++ pexels.js
@@ -2,15 +2,15 @@
 
 const loadPictures = function (query) {
   const URL = `https://api.pexels.com/v1/search?query=${query}`
+  const API_KEY = 'xFBj2MpOtMvpxx12EkG31qsXeIc43DZXkQVqOM5STgLlvhYYTHKhXdBl'
   const row = document.getElementById('card-row')
-  row.innerHTML = '' //clear the webpage
 
   fetch(URL, {
     method: 'GET',
     body: JSON.stringify(),
     headers: {
       'Content-Type': 'application/json',
-      Authorization: 'xFBj2MpOtMvpxx12EkG31qsXeIc43DZXkQVqOM5STgLlvhYYTHKhXdBl',
+      Authorization: API_KEY,
     },
   })
     .then((response) => {
@@ -24,12 +24,15 @@ const loadPictures = function (query) {
     .then((data) => {
       console.log(data)
 
+      row.innerHTML = '' //clear the webpage
+
       // generate a set of cards
 
       data.photos.forEach((pic) => {
         const col = document.createElement('div')
         col.classList.add('col-md-4')
         const card = document.createElement('div')
+
         card.classList.add('card', 'mb-4', 'shadow-sm')
 
         card.innerHTML = `
@@ -39,7 +42,7 @@ const loadPictures = function (query) {
         <div class="card-body">
         <div class="d-flex justify-content-between align-items-center">
         <div class="btn-group">
-        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+        <button type="button" class="btn btn-sm btn-outline-secondary view-btn">View</button>
         <button type="button" class="btn btn-sm btn-outline-secondary hide-btn">Hide</button>
         </div>
         <small class="text-muted font-monospace">id:${pic.id}</small>
@@ -53,6 +56,34 @@ const loadPictures = function (query) {
 
         card.querySelector('.hide-btn').addEventListener('click', () => {
           col.remove()
+        })
+
+        //view button
+        card.querySelector('.view-btn').addEventListener('click', () => {
+          const modalContainer = document.createElement('div')
+          modalContainer.innerHTML = `<div class="modal fade" id="viewImageModal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title font-monospace" id="modalLabel">${pic.alt}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <img src="${pic.src.original}" class="img-fluid">
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      `
+
+          document.body.appendChild(modalContainer)
+          const modal = new bootstrap.Modal(
+            modalContainer.querySelector('.modal')
+          )
+          modal.show()
         })
       })
     })
@@ -71,5 +102,11 @@ window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('second-set').addEventListener('click', (event) => {
     event.preventDefault()
     loadPictures('architecture')
+  })
+  document.getElementById('search-btn').addEventListener('click', (event) => {
+    event.preventDefault()
+    const searchTerm = document.getElementById('search-input').value.trim()
+    loadPictures(searchTerm)
+    document.getElementById('search-form').reset()
   })
 })
